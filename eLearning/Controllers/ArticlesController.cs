83f40ed936804264461753d4374c061866aadf3b
Frank D'Arnese <frankdarnese@gmail.com>
2016-09-09 14:28:56 +0100

    Improved Navigation and logic

--- eLearning/Controllers/ArticlesController.cs
+++ eLearning/Controllers/ArticlesController.cs
@@ -8,13 +8,6 @@ namespace eLearning.Controllers
 {
     public class ArticlesController : BaseController
     {
-        // GET: Articles
-        public ActionResult Index()
-        {
-            var all = DataRepo.AllArticles.ToArray();
-            return View();
-        }
-
         public ActionResult Article(string id, string chapterId)
         {
             var article = DataRepo.AllArticles.FirstOrDefault(x => x.Uid == id);
@@ -23,20 +16,22 @@ namespace eLearning.Controllers
 
             if (string.IsNullOrWhiteSpace(chapterId))
                 chapterId = article.Chapters.FirstOrDefault(x => string.IsNullOrWhiteSpace(x.Separator))?.Uid;
-
+            
             var chapter = article.Chapters.FirstOrDefault(x => x.Uid == chapterId);
             if (chapter == null)
                 throw new HttpException(404, "Chapter not found");
 
-            var index = article.Chapters.ToList().IndexOf(chapter);
+            var chaptersWithoutSeparators = article.Chapters.Where(x => string.IsNullOrWhiteSpace(x.Separator)).ToList();
+            var index = chaptersWithoutSeparators.ToList().IndexOf(chapter);
+
             var vm = new ChapterVM()
             {
                 Uid = chapter.Uid,
                 ArticleUid = article.Uid,
                 Display = chapter.Display,
                 ContentHtml = Db.Chapter.GetHtmlContent(HttpContext, chapter),
-                PreviousUid = index > 0 ? article.Chapters[index - 1].Uid : string.Empty,
-                NextUid = index < article.Chapters.Length - 1 ? article.Chapters[index + 1].Uid : string.Empty
+                PreviousUid = index > 0 ? chaptersWithoutSeparators[index - 1].Uid : string.Empty,
+                NextUid = index < chaptersWithoutSeparators.Count - 1 ? chaptersWithoutSeparators[index + 1].Uid : string.Empty
             };
             return View(vm);
         }
